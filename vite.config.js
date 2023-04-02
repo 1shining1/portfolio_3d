2144d272587f23c2f3ace0c8f928ae1896df7c23
Shining <shining2327@gmail.com>
2023-04-02 15:57:29 -0400

    edit base address

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://github.com/1shining1/portfolio_3d/",
+    base: "https://1shining1.github.io/portfolio_3d/",
 });
